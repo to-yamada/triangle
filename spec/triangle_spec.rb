b63307c811c609f8dc0a5f4e44402ce9718e446b
to-yamada <5902143+to-yamada@users.noreply.github.com>
2018-04-24 02:20:05 +0900

    三角形の形を求めるプログラム、テスト実装

---
+++ spec/triangle_spec.rb
@@ -0,0 +1,60 @@
+require File.expand_path(File.dirname(__FILE__) + '/../triangle')
+
+describe Triangle do
+  describe '#triangle?' do
+    it '短い2辺の和が残りの辺よりも長ければ三角形' do
+      expect(Triangle.new(1, 1.1, 2)).to be_triangle
+    end
+
+    it '短い2辺の和が残りの辺と同じ長さであれば三角形でない' do
+      expect(Triangle.new(1, 1, 2)).not_to be_triangle
+    end
+
+    it '0が含まれる場合三角形ではない' do
+      expect(Triangle.new(0, 1, 1)).not_to be_triangle
+    end
+
+    it '負値が含まれる場合も三角形ではない' do
+      expect(Triangle.new(-1, 1, 0)).not_to be_triangle
+    end
+  end
+
+  describe '#isosceles_triangle?' do
+    it 'いずれかの2辺が等しければ二等辺三角形' do
+      expect(Triangle.new(1, 2, 2)).to be_isosceles_triangle
+    end
+
+    it 'いずれかの2辺は等しいが、三角形の条件を満たさない場合は二等辺三角形ではない' do
+      expect(Triangle.new(1, 1, 2)).not_to be_isosceles_triangle
+    end
+  end
+
+  describe '#equilateral_triangle?' do
+    it '3辺が等しければ正三角形' do
+      expect(Triangle.new(1, 1, 1)).to be_equilateral_triangle
+    end
+
+    it '3辺が等しいが、三角形の条件を満たさない場合は正三角形ではない' do
+      expect(Triangle.new(0, 0, 0.0)).not_to be_equilateral_triangle
+    end
+  end
+
+  describe '#to_s' do
+    it '正三角形ならば正三角形の文字列' do
+      expect(Triangle.new(1, 1, 1).to_s).to eq '正三角形ですね！'
+    end
+
+    it '二等辺三角形ならば二等辺三角形の文字列' do
+      expect(Triangle.new(1, 2, 2).to_s).to eq '二等辺三角形ですね！'
+    end
+
+    it '不等辺三角形ならば不等辺三角形の文字列' do
+      expect(Triangle.new(1, 1.1, 2).to_s).to eq '不等辺三角形ですね！'
+    end
+
+    it '三角形でないならば三角形でない文字列' do
+      expect(Triangle.new(0, 0, 0).to_s).to eq '三角形じゃないです＞＜'
+    end
+  end
+end
+
---
+++ triangle.rb
@@ -0,0 +1,38 @@
+class Triangle
+  def initialize(a, b, c)
+    @a, @b, @c = [a, b, c].sort!
+  end
+
+  def to_s
+    return '正三角形ですね！' if equilateral_triangle?
+    return '二等辺三角形ですね！' if isosceles_triangle?
+    return '不等辺三角形ですね！' if triangle?
+    '三角形じゃないです＞＜'
+  end
+
+  def equilateral_triangle?
+    # 三角形の条件を満たし、かつ全ての辺が等しい
+    triangle? && @a == @b && @b == @c
+  end
+
+  def isosceles_triangle?
+    # 三角形の条件を満たし、かついずれかの2辺が等しい
+    # @a <= @b <= @c であるため、@a == @c はチェック不要
+    triangle? && (@a == @b || @b == @c)
+  end
+
+  def triangle?
+    # 3辺が全て 0 よりも大きく
+    # 短い2辺の和が残りの辺よりも長ければ三角形
+    # @a <= @b <= @c であるため、@a > 0 であれば他の2辺も0より大きい
+    @a > 0 && @a + @b > @c
+  end
+end
+
+if $0 == __FILE__
+  if ARGV.size == 3
+    puts Triangle.new(*ARGV.map(&:to_f)).to_s
+  else
+    puts "#{$0} 数字, 数字, 数字 の形で実行してください。"
+  end
+end
